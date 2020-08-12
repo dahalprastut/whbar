6ef0bcae46b1171e4f5ba9294f4e750eec6b6fc4
dahalprastut <dahalprastut@gmail.com>
2020-08-13 03:46:38 +0545

    first commit and first phase completed

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const ethBtn = document.querySelector('.eth-btn');
+const hbarBtn = document.querySelector('.hbar-btn');
+const status = document.querySelector('.status');
+const operation = document.querySelector('.operation');
+
+ethBtn.addEventListener('click', () => {
+    hbarBtn.classList.remove('active');
+    ethBtn.classList.add('active');
+    status.innerText = 'ETH';
+    operation.innerText = 'Unwrap';
+})
+
+hbarBtn.addEventListener('click', () => {
+    ethBtn.classList.remove('active');
+    hbarBtn.classList.add('active');
+    status.innerText = 'HBAR';
+    operation.innerText = 'Wrap';
+
+
+})
+
