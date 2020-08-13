937150a701014140a2cfe0ea8b5ad405be5dbd25
dahalprastut <dahalprastut@gmail.com>
2020-08-13 18:02:51 +0545

    spacing fixed

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const hbarBtn = document.querySelector('.hbar-btn');
 const status = document.querySelector('.status');
 const operation = document.querySelector('.operation');
 
+
 ethBtn.addEventListener('click', () => {
     hbarBtn.classList.remove('active');
     ethBtn.classList.add('active');
@@ -19,3 +20,17 @@ hbarBtn.addEventListener('click', () => {
 
 })
 
+const loginBtns = document.querySelectorAll('.nav-right_login a');
+const loginBtnsArr = Array.prototype.slice.call(loginBtns);
+
+loginBtnsArr.forEach(element => {
+    element.addEventListener('mouseover', () => {
+        for (let i = 0; i < loginBtnsArr.length; i++) {
+
+
+            loginBtnsArr[i].style.borderBottom = 'none';
+        }
+        element.style.borderBottom = '0.3rem solid #6236FF';
+    })
+
+});
