d3c4e1c8c9101559abfddfef818c5ee25c386b23
dahalprastut <dahalprastut@gmail.com>
2020-08-13 19:54:43 +0545

    second page completed

--- js/main.js
+++ js/main.js
@@ -20,17 +20,4 @@ hbarBtn.addEventListener('click', () => {
 
 })
 
-const loginBtns = document.querySelectorAll('.nav-right_login a');
-const loginBtnsArr = Array.prototype.slice.call(loginBtns);
 
-loginBtnsArr.forEach(element => {
-    element.addEventListener('mouseover', () => {
-        for (let i = 0; i < loginBtnsArr.length; i++) {
-
-
-            loginBtnsArr[i].style.borderBottom = 'none';
-        }
-        element.style.borderBottom = '0.3rem solid #6236FF';
-    })
-
-});
